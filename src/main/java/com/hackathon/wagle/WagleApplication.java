f5e49f65a06870504a2b2c0936d7da0119683dff
koreaioi <koreaioi@gachon.ac.kr>
2025-02-21 15:01:09 +0900

    build: 초기 세팅 start.spring.io build

---
+++ src/main/java/com/hackathon/wagle/WagleApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.wagle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WagleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/wagle/WagleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.wagle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WagleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
