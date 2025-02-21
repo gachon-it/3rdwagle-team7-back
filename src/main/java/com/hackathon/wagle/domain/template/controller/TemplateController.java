af29458033bdc751bb24eb699f89e5e9cefcdac4
leewonhee-3054 <dldnjsgml3054@naver.com>
2025-02-22 01:33:00 +0900

    fix: template 인수부분 User 객체로 변경

--- src/main/java/com/hackathon/wagle/domain/template/controller/TemplateController.java
+++ src/main/java/com/hackathon/wagle/domain/template/controller/TemplateController.java
@@ -2,6 +2,8 @@ package com.hackathon.wagle.domain.template.controller;
 
 import com.hackathon.wagle.domain.template.dto.TemplateRequestDto;
 import com.hackathon.wagle.domain.template.service.TemplateService;
+import com.hackathon.wagle.domain.user.entity.User;
+import com.hackathon.wagle.domain.user.service.UserService;
 import com.hackathon.wagle.global.common.response.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,17 +17,23 @@ public class TemplateController {
     @Autowired
     private TemplateService templateService;
 
+    @Autowired
+    private UserService userService;
+
     // 특정 템플릿 조회 및 값 치환
     @GetMapping("/{name}")
     public ApiResponse<String> getFormattedTemplate(
-            @PathVariable String name,
+            @PathVariable String name, // 카테고리 목록(성적정정, 수강빌넣 등)
             @RequestParam String professorName,
-            @RequestParam String department,
-            @RequestParam String studentNumber,
-            @RequestParam String studentName,
-            @RequestParam String courseName) {
+            @RequestParam String courseName,
+            @RequestParam String studentNumber
+             ) {
 
-        String formattedTemplate = templateService.getFormattedTemplate(name, professorName, department, studentNumber ,studentName, courseName);
+        User user = userService.findByStudentNumber(studentNumber);
+
+        String formattedTemplate = templateService.getFormattedTemplate(
+                name, professorName, user.getMajor(), user.getStudentNumber(),
+                user.getUsername(), courseName);
         return ApiResponse.response(HttpStatus.OK, "템플릿을 성공적으로 반환했습니다!", formattedTemplate);
     }
 
