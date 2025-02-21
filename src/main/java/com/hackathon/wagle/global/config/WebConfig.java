73ebff3ae719bcd623cd5cd46f49d2aa71f224b4
koreaioi <koreaioi@gachon.ac.kr>
2025-02-22 01:12:33 +0900

    HOTFIX: CORS 오류 해결 도전 2

--- src/main/java/com/hackathon/wagle/global/config/WebConfig.java
+++ src/main/java/com/hackathon/wagle/global/config/WebConfig.java
@@ -13,12 +13,11 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                        .allowedOrigins("*") // 모든 도메인 허용 (보안이 필요하면 특정 도메인만 입력)
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "OPTIONS") // 허용할 HTTP 메서드
-                        .allowedHeaders("*") // 모든 헤더 허용
-                        .exposedHeaders("")
-                        .allowCredentials(true); // 쿠키 포함 요청 허용
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") // 특정 도메인 허용
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // OPTIONS 메서드 추가
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // 자격 증명 허용
             }
         };
     }
