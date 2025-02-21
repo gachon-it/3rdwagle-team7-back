a585702329f69da44d9c03c499396f9f93a08a40
KwonLaptop <chris03232000@gmail.com>
2025-02-22 02:38:02 +0900

    feat: 사용자 입력 정보와 메일 유형 정해서 메일 대신 전송

--- src/main/java/com/hackathon/wagle/domain/email/EmailController.java
+++ src/main/java/com/hackathon/wagle/domain/email/EmailController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.hackathon.wagle.domain.template.entity.Template;
+import com.hackathon.wagle.domain.template.service.TemplateService;
 import com.hackathon.wagle.domain.user.entity.User;
 import com.hackathon.wagle.domain.user.service.UserService;
 
@@ -22,6 +24,9 @@ public class EmailController {
 	
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private TemplateService templateService;
 	
 	@GetMapping("/change-picture-form")
 	public String changePicture() {
@@ -44,7 +49,7 @@ public class EmailController {
 			User user = userService.findByStudentNumber(studentNumber);
 			
 			// 내용
-			String userName = user.getUsername(); 
+			String userName = user.getUsername();
 			String major = user.getMajor();
 			
 			String emailBody = studentNumber + major + userName + "입니다 \n" 
@@ -67,5 +72,47 @@ public class EmailController {
 	}
 	
 	
+	// 유형 선택해서 이메일 보내기
+	@PostMapping("/send-email")
+	public String sendEmail(@RequestParam("name") String name
+	                        , @RequestParam("professorName") String professorName
+	                        , @RequestParam("department") String department
+	                        , @RequestParam("studentNumber") String studentNumber
+	                        , @RequestParam("courseName") String courseName) {
+	    
+	    User user = userService.findByStudentNumber(studentNumber);
+	    String userName = user.getUsername();
+	    String major = user.getMajor();
+	    String studentEmail =  "chris0540@naver.com"; //user.getEmail();  
+	    
+	    
+	    String template = templateService.getFormattedTemplate(name, professorName, department, studentNumber, userName, courseName);
+
+	    // 이메일 제목 설정
+	    String subject = professorName + " 교수님에게 보내는 성적 이의 제기 요청";
+
+	    try {
+	    	
+	        MimeMessage message = mailSender.createMimeMessage();
+	        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+	        
+	        // 나중에 교수님 메일 주소로 교체하기
+	        helper.setTo(studentEmail);  
+
+	        helper.setSubject(subject);
+
+	        helper.setText(template, true);  // true를 설정하면 HTML 콘텐츠로 처리됨
+
+	        // 이메일 전송
+	        mailSender.send(message);
+
+	        return "successPage";  // 성공 페이지로 리다이렉트
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return "errorPage";  // 오류 발생 시 오류 페이지로 리다이렉트
+	    }
+	}
+
 	
 }
--- src/main/java/com/hackathon/wagle/domain/email/service/EmailService.java
+++ src/main/java/com/hackathon/wagle/domain/email/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.hackathon.wagle.domain.email.service;
 
 import java.io.File;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
@@ -9,6 +10,8 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.hackathon.wagle.domain.email.dto.EmailDetail;
+import com.hackathon.wagle.domain.template.entity.Template;
+import com.hackathon.wagle.domain.template.repository.TemplateRepository;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
@@ -19,7 +22,11 @@ public class EmailService {
 	@Autowired
 	private JavaMailSender mailSender;
 		
+	@Autowired
+	private TemplateRepository templateRepository;
 	
+	
+	// 프로필 사진 변경
 	public void sendEmailWithAttachmentToFixedRecipient(EmailDetail emailDetail, String fixedRecipient) throws MessagingException{
 		
 		MimeMessage message = mailSender.createMimeMessage();
@@ -33,6 +40,28 @@ public class EmailService {
 		helper.addAttachment(file.getFilename(), file);
 		
 		mailSender.send(message);
+	}
+	
+	
+	
+	
+	// 성적 이의 제기
+	public void sendEmailWithScoreObject(String name) throws MessagingException {
+		
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+		
+		Optional<Template> optionalTemplate = templateRepository.findByName(name);
+		Template template = optionalTemplate.orElse(null);
+		
+		helper.setTo("chris0540@naver.com");
+		
+		helper.setSubject(template.getName());
+		
+		helper.setText(template.getContent());
+		
+		mailSender.send(message);
+		
 	}
 	
 	
