5d610a9b108c21c2e1f589ecd7f98e64efb5cd11
KwonLaptop <chris03232000@gmail.com>
2025-02-21 22:05:52 +0900

    feat: 사진 첨부하여 지정된 이메일에 보내기

--- src/main/java/com/hackathon/wagle/domain/email/EmailController.java
+++ src/main/java/com/hackathon/wagle/domain/email/EmailController.java
@@ -5,6 +5,9 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.mail.internet.MimeMessage;
 
@@ -14,8 +17,13 @@ public class EmailController {
 	@Autowired
 	private JavaMailSender mailSender;
 	
-	@GetMapping("/send")
-	public String sendTestEmail() {
+	@GetMapping("/change-picture-form")
+	public String changePicture() {
+		return "sendPicture.html";
+	}
+	
+	@PostMapping("/send")
+	public String sendTestEmail(@RequestParam("file") MultipartFile file) {
 		try {
 			MimeMessage message = mailSender.createMimeMessage();
 			MimeMessageHelper helper = new MimeMessageHelper(message,true, "UTF-8");
@@ -24,8 +32,13 @@ public class EmailController {
 			helper.setSubject("테스트 이메일");
 			helper.setText("Spring Boot에서 보낸 이메일입니다.", true);
 			
+			// 파일 첨부 추가
+			if(file != null && !file.isEmpty()) {
+				helper.addAttachment(file.getOriginalFilename(), file);
+			}
+			
 			mailSender.send(message);
-			return "이메일 전송 성공";
+			return "redirect:https://www.naver.com";
 		} catch (Exception e) {
 			e.printStackTrace();
 			return "이메일 전송 실패: " + e.getMessage();
