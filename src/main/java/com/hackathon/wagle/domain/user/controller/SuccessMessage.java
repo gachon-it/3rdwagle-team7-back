dee1dbfe86ea7d0e49e18dc85acc527362116c40
koreaioi <koreaioi@gachon.ac.kr>
2025-02-21 20:35:05 +0900

    feat: User SuccessMessage 추가

---
+++ src/main/java/com/hackathon/wagle/domain/user/controller/SuccessMessage.java
@@ -0,0 +1,13 @@
+package com.hackathon.wagle.domain.user.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessMessage {
+
+    USER_CREATED("회원가입에 성공했습니다!");
+
+    private final String message;
+}
