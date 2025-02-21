f2d0aa4d066b2ebc70344aea0952ef775c1bb605
koreaioi <koreaioi@gachon.ac.kr>
2025-02-21 21:35:51 +0900

    feat: 로그인 비지니스 로직 추가

--- src/main/java/com/hackathon/wagle/domain/user/controller/UserController.java
+++ src/main/java/com/hackathon/wagle/domain/user/controller/UserController.java
@@ -1,7 +1,8 @@
 package com.hackathon.wagle.domain.user.controller;
 
 import com.hackathon.wagle.domain.user.dto.request.UserRequestDto;
-import com.hackathon.wagle.domain.user.entity.User;
+import com.hackathon.wagle.domain.user.dto.request.UserLoginDto;
+import com.hackathon.wagle.domain.user.dto.response.LoginDto;
 import com.hackathon.wagle.domain.user.service.UserService;
 import com.hackathon.wagle.global.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.hackathon.wagle.domain.user.controller.SuccessMessage.USER_CREATED;
+import static com.hackathon.wagle.domain.user.controller.SuccessMessage.USER_LOGIN;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
@@ -24,6 +26,15 @@ public class UserController {
         userService.saveUser(dto);
 
         return ApiResponse.response(OK, USER_CREATED.getMessage());
+    }
+
+    @PostMapping("/api/login")
+    public ApiResponse<LoginDto> login(@RequestBody UserLoginDto dto) {
+
+        LoginDto response = userService.login(dto);
+
+
+        return ApiResponse.response(OK, USER_LOGIN.getMessage(), response);
     }
 
 }
--- src/main/java/com/hackathon/wagle/domain/user/service/UserService.java
+++ src/main/java/com/hackathon/wagle/domain/user/service/UserService.java
@@ -1,11 +1,14 @@
 package com.hackathon.wagle.domain.user.service;
 
 import com.hackathon.wagle.domain.user.dto.request.UserRequestDto;
+import com.hackathon.wagle.domain.user.dto.request.UserLoginDto;
+import com.hackathon.wagle.domain.user.dto.response.LoginDto;
+import com.hackathon.wagle.domain.user.dto.response.UserResponseDto;
 import com.hackathon.wagle.domain.user.entity.User;
 import com.hackathon.wagle.domain.user.exception.DuplicatedStudentNumberException;
 import com.hackathon.wagle.domain.user.exception.UserNotFoundException;
 import com.hackathon.wagle.domain.user.repository.UserRepository;
-import com.hackathon.wagle.test.TestException;
+import com.hackathon.wagle.global.auth.jwt.utils.JwtProvider;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
 
     @Transactional
     public void saveUser(UserRequestDto dto) {
@@ -26,8 +30,31 @@ public class UserService {
         userRepository.save(user);
     }
 
+    public LoginDto login(UserLoginDto dto) {
+        User user = findByStudentNumber(dto.studentNumber());
+        String token = generateToken(user);
+
+        return LoginDto.of(UserResponseDto.from(user), token);
+    }
+
+
+    /*
+    * refactor
+    * */
+
+    public String generateToken(User user) {
+        return jwtProvider.generateAccessToken(
+                user.getId(), user.getEmail(), user.getRole().name()
+        );
+    }
+
     public User findById(Long id) {
         return userRepository.findById(id)
+                .orElseThrow(UserNotFoundException::new);
+    }
+
+    public User findByStudentNumber(String studentNumber) {
+        return userRepository.findByStudentNumber(studentNumber)
                 .orElseThrow(UserNotFoundException::new);
     }
 
