5acb37e38e9d439bb5d1d03f26637240501498bc
koreaioi <koreaioi@gachon.ac.kr>
2025-02-22 05:02:20 +0900

    feat: 이메일 템플릿 생성

--- src/main/java/com/hackathon/wagle/domain/template/controller/TemplateController.java
+++ src/main/java/com/hackathon/wagle/domain/template/controller/TemplateController.java
@@ -1,5 +1,6 @@
 package com.hackathon.wagle.domain.template.controller;
 
+import com.hackathon.wagle.domain.email.dto.EmailResponseDto;
 import com.hackathon.wagle.domain.template.dto.TemplateRequestDto;
 import com.hackathon.wagle.domain.template.service.TemplateService;
 import com.hackathon.wagle.domain.user.entity.User;
@@ -21,9 +22,9 @@ public class TemplateController {
     private UserService userService;
 
     // 특정 템플릿 조회 및 값 치환
-    @GetMapping("/{name}")
+    @GetMapping
     public ApiResponse<String> getFormattedTemplate(
-            @PathVariable String name, // 카테고리 목록(성적정정, 수강빌넣 등)
+            @RequestParam String name, // 카테고리 목록(성적정정, 수강빌넣 등)
             @RequestParam String professorName,
             @RequestParam String courseName,
             @RequestParam String studentNumber
@@ -40,5 +41,25 @@ public class TemplateController {
     @PostMapping
     public ApiResponse<Void> createTemplate(TemplateRequestDto dto) {
         return ApiResponse.response(HttpStatus.OK, "템플릿을 성공적으로 반환했습니다!");
+    }
+
+    @GetMapping("/student-photo")
+    public ApiResponse<EmailResponseDto> getStudentPhoto(
+            @RequestParam String studentNumber
+    ){
+        User user = userService.findByStudentNumber(studentNumber);
+
+        String title = "모바일학생증 사진 교체 부탁드립니다.";
+        String content = "안녕하세요." +
+                "\n" +
+                user.getStudentNumber() + " " + user.getMajor() + " " + user.getUsername() + "입니다." +
+                "\n" +
+                "학생증에 있는 사진이 너무 못생기게 나와서 교체 부탁드려요!" +
+                "\n" +
+                "감사합니다";
+
+        EmailResponseDto response = EmailResponseDto.of(title, content);
+
+        return ApiResponse.response(HttpStatus.OK, title, response);
     }
 }
